afd6ac4dcbba80fffeae20b25cc4489a1dcbef2b
Arthur Hej <82497746+arthurhej@users.noreply.github.com>
2021-05-07 13:46:22 +0200

    created example class

---
+++ src/main/java/de/itech/it9a/b4/aeproj/gruppe2/spaceinvclonebackend/example/Example.java
@@ -0,0 +1,10 @@
+package de.itech.it9a.b4.aeproj.gruppe2.spaceinvclonebackend.example;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
