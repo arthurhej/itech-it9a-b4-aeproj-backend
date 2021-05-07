6186926397ff8b8c987fca5db4d9b96bbc20e7ad
Florian Lutz <lutz.florian@outlook.de>
2021-05-07 11:29:17 +0200

    backend codebase

---
+++ src/main/java/de/itech/it9a/b4/aeproj/gruppe2/spaceinvclonebackend/SpaceInvCloneBackendApplication.java
@@ -0,0 +1,13 @@
+package de.itech.it9a.b4.aeproj.gruppe2.spaceinvclonebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceInvCloneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceInvCloneBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/itech/it9a/b4/aeproj/gruppe2/spaceinvclonebackend/SpaceInvCloneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.itech.it9a.b4.aeproj.gruppe2.spaceinvclonebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceInvCloneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
