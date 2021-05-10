9bf3b9d8557dc0fb7400022ca2efacd14411fa7d
Kruse, Leon <kruseleon@hotmail.de>
2021-05-10 15:18:35 +0200

    Rest approach

--- src/main/java/de/itech/it9a/b4/aeproj/gruppe2/spaceinvclonebackend/api/RestService.java
+++ src/main/java/de/itech/it9a/b4/aeproj/gruppe2/spaceinvclonebackend/api/RestService.java
@@ -1,5 +1,23 @@
 package de.itech.it9a.b4.aeproj.gruppe2.spaceinvclonebackend.api;
 
-public class RestService {
+import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import de.itech.it9a.b4.aeproj.gruppe2.spaceinvclonebackend.controller.ScoreController;
+import de.itech.it9a.b4.aeproj.gruppe2.spaceinvclonebackend.model.Score;
+
+@RestController
+public class RestService {
+	
+	private ScoreController scoreController;
+	
+	public RestService() {
+		this.scoreController = new ScoreController();
+	}
+	
+	@GetMapping("/scores")
+	private List<Score> newScore(){
+	}
 }
