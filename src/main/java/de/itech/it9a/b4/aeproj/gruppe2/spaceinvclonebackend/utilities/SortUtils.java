61f9537dc7084ef5a42fe84ea8ed1c5311082419
Arthur Hej <82497746+arthurhej@users.noreply.github.com>
2021-05-27 14:35:22 +0200

    comments

--- src/main/java/de/itech/it9a/b4/aeproj/gruppe2/spaceinvclonebackend/utilities/SortUtils.java
+++ src/main/java/de/itech/it9a/b4/aeproj/gruppe2/spaceinvclonebackend/utilities/SortUtils.java
@@ -5,8 +5,17 @@ import java.util.List;
 
 import de.itech.it9a.b4.aeproj.gruppe2.spaceinvclonebackend.model.Score;
 
+/**
+ * Utility class with specific methods
+ */
 public class SortUtils {
 
+    /**
+     * receives a list of scores, sorts it by points (descending) and returns the list
+     * 
+     * @param listOfScores
+     * @return sorted List
+     */
     public static List<Score> myShortSort(List<Score> listOfScores) {
 
         listOfScores.sort(Comparator.comparingLong(Score::getPoints).reversed());
