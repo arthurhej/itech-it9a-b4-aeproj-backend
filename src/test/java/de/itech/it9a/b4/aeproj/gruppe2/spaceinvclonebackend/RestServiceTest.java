7d55e137e41dacf4b7e57e21cb90f9a2b0e45b67
Arthur Hej <82497746+arthurhej@users.noreply.github.com>
2021-05-30 14:06:59 +0200

    full formatter run

--- src/test/java/de/itech/it9a/b4/aeproj/gruppe2/spaceinvclonebackend/RestServiceTest.java
+++ src/test/java/de/itech/it9a/b4/aeproj/gruppe2/spaceinvclonebackend/RestServiceTest.java
@@ -1,11 +1,11 @@
 package de.itech.it9a.b4.aeproj.gruppe2.spaceinvclonebackend;
 
-import de.itech.it9a.b4.aeproj.gruppe2.spaceinvclonebackend.api.RestService;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import de.itech.it9a.b4.aeproj.gruppe2.spaceinvclonebackend.api.RestService;
 
 @WebMvcTest(RestService.class)
 public class RestServiceTest {
--- src/test/java/de/itech/it9a/b4/aeproj/gruppe2/spaceinvclonebackend/SortUtilsTest.java
+++ src/test/java/de/itech/it9a/b4/aeproj/gruppe2/spaceinvclonebackend/SortUtilsTest.java
@@ -2,28 +2,33 @@ package de.itech.it9a.b4.aeproj.gruppe2.spaceinvclonebackend;
 
 import static de.itech.it9a.b4.aeproj.gruppe2.spaceinvclonebackend.utilities.SortUtils.myShortSort;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.verify;
-
-import de.itech.it9a.b4.aeproj.gruppe2.spaceinvclonebackend.model.Score;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import de.itech.it9a.b4.aeproj.gruppe2.spaceinvclonebackend.model.Score;
+import de.itech.it9a.b4.aeproj.gruppe2.spaceinvclonebackend.utilities.SortUtils;
+
+/**
+ * Unit tests for {@link SortUtils}
+ */
 @SpringBootTest
-public class SortUtilsTest {
+class SortUtilsTest {
 
     @Test
-    public void testMyShortSortSortsCorrectly(){
-        List<Score> mockReturnList = Arrays.asList(new Score("Test1", 1L), new Score("Test2", 2L), new Score("Test3", 3L), new Score("Test4", 4L),
-                new Score("Test5", 5L), new Score("Test6", 6L), new Score("Test7", 7L), new Score("Test8", 8L), new Score("Test9", 9L),
-                new Score("Test10", 10L));
+    void testMyShortSortSortsCorrectly() {
+
+        List<Score> mockReturnList = Arrays.asList(new Score("Test1", 1L), new Score("Test2", 2L), new Score("Test3", 3L),
+                new Score("Test4", 4L), new Score("Test5", 5L), new Score("Test6", 6L), new Score("Test7", 7L), new Score("Test8", 8L),
+                new Score("Test9", 9L), new Score("Test10", 10L));
 
         long scoreValue = 10L;
         List<Score> sortedScoreList = myShortSort(mockReturnList);
 
-        for(Score score : sortedScoreList){
+        for (Score score : sortedScoreList) {
             assertEquals("Test" + scoreValue, score.getName());
             assertEquals(scoreValue, score.getPoints());
             scoreValue--;
